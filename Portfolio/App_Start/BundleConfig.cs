d4646e60612dfdb67c6e9e2a1da7bf7f64d68baf
tam.s89 <tam.s89@gmail.com>
2013-10-18 21:06:06 +0100

    ++

--- Portfolio/App_Start/BundleConfig.cs
+++ Portfolio/App_Start/BundleConfig.cs
@@ -4,18 +4,13 @@ namespace Portfolio.App_Start
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/angularbootstrap").Include(
+                      "~/Scripts/angular.js",
                       "~/Scripts/angular-*",
                       "~/Scripts/ui-bootstrap-*",
                       "~/Scripts/respond.js"));

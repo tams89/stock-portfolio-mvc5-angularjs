d354cc9acc9bcf4741493149e6f6364bd223207d
tam.s89 <tam.s89@gmail.com>
2013-10-19 12:41:49 +0100

    ++

---
+++ Core/ORM/Mappings/Portfolio/PortfolioMap.cs
@@ -0,0 +1,16 @@
+﻿using Core.Models.HFT;
+using DapperExtensions.Mapper;
+
+namespace Core.ORM.Mappings.Portfolio
+{
+    public class PortfolioMap : ClassMapper<Models.Portfolio.Portfolio>
+    {
+        public PortfolioMap()
+        {
+            base.Schema("Portfolio");
+            base.Table("Portfolio");
+            Map(f => f.PortfolioId).Key(KeyType.Guid);
+            Map(f => f.UserId).Column("UserId");
+        }
+    }
+}
---
+++ Core/Services/ServiceBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.ORM;
+using DapperExtensions;
+
+namespace Core.Services
+{
+    public class ServiceBase<T> where T : class, new()
+    {
+        /// <summary>
+        /// Gets all data pertaining to the specified type.
+        /// </summary>
+        /// <returns>Collection of tick data.</returns>
+        public IEnumerable<T> GetAll()
+        {
+            using (var c = new SqlConnection(Constants.AlgoTradingDbConnectionStr))
+            {
+                c.Open();
+                var entity = c.GetList<T>(buffered: true);
+                c.Close();
+
+                return entity;
+            }
+        }
+    }
+}

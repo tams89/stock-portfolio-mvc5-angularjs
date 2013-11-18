b03c2c4f3afc4c3cf73a0234750f20aa6b916d3e
tam.s89 <tam.s89@gmail.com>
2013-11-18 21:30:21 +0000

    - Refactored scripts and styles into partial view _dependencies in shared folder

--- Portfolio/App_Start/BundleConfig.cs
+++ Portfolio/App_Start/BundleConfig.cs
@@ -1,79 +1,71 @@
-﻿using System.Web.Optimization;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BundleConfig.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The bundle config.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace Portfolio.App_Start
 {
+    using System.Web.Optimization;
+
+    /// <summary>
+    /// The bundle config.
+    /// </summary>
     public static class BundleConfig
     {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The register bundles.
+        /// </summary>
+        /// <param name="bundles">
+        /// The bundles.
+        /// </param>
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/Styles/angular-csp.css",
-                "~/Content/Styles/bootstrap.css",
-                "~/Content/Styles/bootstrap-buttons.css",
-                "~/Content/Styles/toaster.css",
-                "~/Content/Styles/spinner.css",
-                "~/Content/Styles/circle.css",
-                "~/Content/Styles/ng-grid.css",
-                "~/Content/Styles/animate.css"
-                ));
+            bundles.Add(
+                new StyleBundle("~/Content/css").Include(
+                    "~/Content/Styles/angular-csp.css", 
+                    "~/Content/Styles/bootstrap.css", 
+                    "~/Content/Styles/bootstrap-buttons.css", 
+                    "~/Content/Styles/toaster.css", 
+                    "~/Content/Styles/spinner.css", 
+                    "~/Content/Styles/circle.css", 
+                    "~/Content/Styles/ng-grid.css", 
+                    "~/Content/Styles/animate.css"));
 
-            bundles.Add(new StyleBundle("~/Content/siteCss").Include(
-                "~/Content/Styles/Site.css"
-                ));
+            bundles.Add(new StyleBundle("~/Content/siteCss").Include("~/Content/Styles/Site.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                "~/Scripts/Modernizer/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/Modernizer/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/ajaxLogin").Include("~/Scripts/App/ajaxLogin.js"));
+            bundles.Add(new ScriptBundle("~/bundles/highStock").IncludeDirectory("~/Scripts/HighStock", "*.js", true));
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include("~/Scripts/Angular/angular.js"));
+            bundles.Add(new ScriptBundle("~/bundles/angularRoute").Include("~/Scripts/Angular/angular-route.js"));
+            bundles.Add(new ScriptBundle("~/bundles/angularAnimate").Include("~/Scripts/Angular/angular-animate.js"));
+            bundles.Add(new ScriptBundle("~/bundles/angularResource").Include("~/Scripts/Angular/angular-resource.js"));
+            bundles.Add(new ScriptBundle("~/bundles/angularScenario").Include("~/Scripts/Angular/angular-scenario.js"));
+            bundles.Add(new ScriptBundle("~/bundles/angularLoader").Include("~/Scripts/Angular/angular-loader.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/Angular/ui-bootstrap-*"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/Jquery/jquery-*"));
+            bundles.Add(
+                new ScriptBundle("~/bundles/jqueryVal").Include(
+                    "~/Scripts/Jquery/Validate/jquery.unobtrusive*", 
+                    "~/Scripts/Jquery/Validate/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/ajaxLogin").Include(
-                "~/Scripts/App/ajaxLogin.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/highStock")
-                .IncludeDirectory("~/Scripts/HighStock", "*.js", true));
-
-            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                "~/Scripts/Angular/angular.js",
-                "~/Scripts/Angular/angular-route.js",
-                "~/Scripts/Angular/angular-animate.js",
-                "~/Scripts/Angular/angular-resource.js",
-                "~/Scripts/Angular/angular-scenario.js",
-                "~/Scripts/Angular/angular-loader.js"
-                ));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/Angular/ui-bootstrap-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/Jquery/jquery-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryVal").Include(
-                "~/Scripts/Jquery/Validate/jquery.unobtrusive*",
-                "~/Scripts/Jquery/Validate/jquery.validate*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/toast").Include(
-                "~/Scripts/Toast/toaster.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/respond").Include(
-                "~/Scripts/Respond/respond.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/underScore").Include(
-                "~/Scripts/UnderScore/underscore-min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/angularApp")
-                .IncludeDirectory("~/Scripts/App", "AlgoTrader.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/angularModules")
-                .IncludeDirectory("~/Scripts/App/Modules", "*.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/angularDirectives")
-                .IncludeDirectory("~/Scripts/App/Directives", "*.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/angularServices")
-                .IncludeDirectory("~/Scripts/App/Services", "*.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/angularControllers")
-                .IncludeDirectory("~/Scripts/App/Controllers", "*.js"));
-
-            BundleTable.EnableOptimizations = false;
+            bundles.Add(new ScriptBundle("~/bundles/toast").Include("~/Scripts/Toast/toaster.js"));
+            bundles.Add(new ScriptBundle("~/bundles/respond").Include("~/Scripts/Respond/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/underScore").Include("~/Scripts/UnderScore/underscore-min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/angularApp").IncludeDirectory("~/Scripts/App", "AlgoTrader.js"));
+            bundles.Add(new ScriptBundle("~/bundles/angularModules").IncludeDirectory("~/Scripts/App/Modules", "*.js"));
+            bundles.Add(new ScriptBundle("~/bundles/angularDirectives").IncludeDirectory("~/Scripts/App/Directives", "*.js"));
+            bundles.Add(new ScriptBundle("~/bundles/angularServices").IncludeDirectory("~/Scripts/App/Services", "*.js"));
+            bundles.Add(new ScriptBundle("~/bundles/angularControllers").IncludeDirectory("~/Scripts/App/Controllers", "*.js"));
+            //BundleTable.EnableOptimizations = false;
         }
+
+        #endregion
     }
 }

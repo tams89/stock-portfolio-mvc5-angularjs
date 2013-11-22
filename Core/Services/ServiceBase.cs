21c039ca839bc06beef9a1c72dfa426dd8115704
tam.s89 <tam.s89@gmail.com>
2013-11-22 21:24:07 +0000

    ++

---
+++ Core/Services/ServiceBase.cs
@@ -0,0 +1,72 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ServiceBase.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The service base.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Security.Principal;
+using DapperExtensions;
+
+namespace Core.Services
+{
+    /// <summary>
+    /// The service base.
+    /// </summary>
+    /// <typeparam name="T">
+    /// </typeparam>
+    public class ServiceBase<T> where T : class, new()
+    {
+        /// <summary>
+        ///     Gets the authenticated user.
+        /// </summary>
+        public string AuthenticatedUser
+        {
+            get
+            {
+                WindowsIdentity currentIdent = WindowsIdentity.GetCurrent();
+                return currentIdent != null ? currentIdent.Name : string.Empty;
+            }
+        }
+
+        /// <summary>
+        ///     Gets all data.
+        /// </summary>
+        /// <returns>Collection of tick data.</returns>
+        public IEnumerable<T> Get()
+        {
+            using (var c = new SqlConnection(Constants.AlgoTraderDbConnection))
+            {
+                c.Open();
+                IEnumerable<T> entity = c.GetList<T>(buffered: true);
+                c.Close();
+                return entity;
+            }
+        }
+
+        /// <summary>
+        /// Gets all data by id.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="T"/>.
+        /// </returns>
+        public T Find(Guid id)
+        {
+            using (var c = new SqlConnection(Constants.AlgoTraderDbConnection))
+            {
+                c.Open();
+                var entity = c.Get<T>(id);
+                c.Close();
+                return entity;
+            }
+        }
+    }
+}

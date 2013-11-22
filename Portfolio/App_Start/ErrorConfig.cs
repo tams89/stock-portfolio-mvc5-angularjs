a10f8ffbb5721c937c225fc4e5921a727a21f597
tam.s89 <tam.s89@gmail.com>
2013-11-22 20:38:56 +0000

    Custom error handling added

---
+++ Portfolio/App_Start/ErrorConfig.cs
@@ -0,0 +1,68 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ErrorConfig.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the ErrorConfig type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Portfolio.App_Start
+{
+    using System.Globalization;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+    using Controllers;
+
+    /// <summary>
+    /// The error config.
+    /// </summary>
+    public class ErrorConfig
+    {
+        /// <summary>
+        /// The handler.
+        /// </summary>
+        /// <param name="context">
+        /// The context.
+        /// </param>
+        public static void Handle(HttpContext context)
+        {
+            switch (context.Response.StatusCode)
+            {
+                // Not authorized.
+                case 401:
+                    Show(context, 401);
+                    break;
+
+                // Not found.
+                case 404:
+                    Show(context, 404);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// The show.
+        /// </summary>
+        /// <param name="context">
+        /// The context.
+        /// </param>
+        /// <param name="code">
+        /// The code.
+        /// </param>
+        private static void Show(HttpContext context, int code)
+        {
+            context.Response.Clear();
+
+            var w = new HttpContextWrapper(context);
+            var c = new ErrorController() as IController;
+            var rd = new RouteData();
+
+            rd.Values["controller"] = "Error";
+            rd.Values["action"] = "Index";
+            rd.Values["id"] = code.ToString(CultureInfo.InvariantCulture);
+
+            c.Execute(new RequestContext(w, rd));
+        }
+    }
+}
---
+++ Portfolio/Controllers/ErrorController.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ErrorController.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The error controller.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Portfolio.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// The error controller.
+    /// </summary>
+    public class ErrorController : Controller
+    {
+        // GET: /Error/
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The index.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        [HttpGet]
+        public ActionResult Index(int? id)
+        {
+            var statusCode = id.HasValue ? id.Value : 500;
+            var error =
+                new HandleErrorInfo(
+                    new Exception(string.Format("Wooo, this is embarrassing, a http {0} error occured.", statusCode)), 
+                    "Error", 
+                    "Index");
+            return View("Error", error);
+        }
+
+        #endregion
+    }
+}
